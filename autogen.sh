8bc350db61a0eb3ec9ed138035e6378866abf280
John Akre <john.w.akre@intel.com>
2018-08-10 02:37:39 +0000

    Add certificate support to testlib
    This commit adds functions to testlib for the following:
    
    - generate_certificate
    - create_trusted_cacert
    - destroy_trusted_cacert
    
    These functions are used to create a self-signed public/private key
    pair, create a trusted key store for the test environment with a public
    key, and to delete the trusted key store. For the test environment to use
    the trusted key store, swupd must be configured using
    --with-fallback-capaths=<path to key store>. This configuration option
    has been added to autogen and travis.
    
    Signed-off-by: John Akre <john.w.akre@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 autoreconf --force --install --symlink --warnings=all
 
 args="\
 --sysconfdir=/etc \
 --localstatedir=/var \
 --prefix=/usr \
---enable-silent-rules"
+--enable-silent-rules \
+--with-fallback-capaths=/tmp/swupd_test_certificates"
 
 ./configure CFLAGS="-g -O2 $CFLAGS" $args "$@"
 make clean
